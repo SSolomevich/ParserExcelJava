a983dc1ab9eb60d9c4e6b58b40552db798af9372
Sergey Solomevich <sergeysolomevich@gmail.com>
2018-09-06 13:13:27 +0300

    commit 5

---
+++ src/main/java/Gradient.java
@@ -0,0 +1,7 @@
+/**
+ * Created by 15 on 04.09.2018.
+ */
+public class Gradient {
+    private double time;
+    private double b;
+}
---
+++ src/main/java/Mid.java
@@ -0,0 +1,33 @@
+/**
+ * Created by 15 on 19.08.2018.
+ */
+public class Mid {
+
+
+    private double b_mid;
+    private double delta_mid;
+
+    public Mid(double b_mid, double delta_mid) {
+
+        this.b_mid = b_mid;
+        this.delta_mid = delta_mid;
+    }
+
+
+
+    public double getB_mid() {
+        return b_mid;
+    }
+
+    public void setB_mid(double b_mid) {
+        this.b_mid = b_mid;
+    }
+
+    public double getDelta_mid() {
+        return delta_mid;
+    }
+
+    public void setDelta_mid(double delta_mid) {
+        this.delta_mid = delta_mid;
+    }
+}
